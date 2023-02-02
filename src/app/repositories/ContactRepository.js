564d4174f9f44bcef876464efd50a360fdfe4496
João Fernandes <joaofergear003@gmail.com>
2023-02-02 14:39:30 -0300

    fix(dependencies/uuid): fix the deprecated version

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require('uuidv4')
+const { v4 } = require('uuid')
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'João',
     email: 'joaofer@gmail.com',
     phone: '838288228',
-    category_id: uuid()
+    category_id: v4()
   }
 ]
 
