942b1eed3d42bebcd1ec8f0586375674bfe476ec
João Fernandes <joaofergear003@gmail.com>
2023-02-02 15:00:50 -0300

    feat(ContactController): create findById()

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -16,6 +16,12 @@ class ContactRepository {
       resolve(contacts)
     })
   }
+  
+  findById(id) {
+    return new Promise(resolve => {
+      resolve(contacts.find(contact => contact.id === id))
+    })
+  }
 }
 
 exports.ContactRepository = new ContactRepository()
