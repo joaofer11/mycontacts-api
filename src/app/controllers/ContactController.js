4f723cc435499a7250395c95ee5d4621f6df220e
João Fernandes <joaofergear003@gmail.com>
2023-02-09 23:48:44 -0300

    perf(ContactController): remove validation code per existing resource

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -87,19 +87,9 @@ class ContactController {
     
     const { id } = req.params
     
-    const contact = await ContactRepository.findById(id)
-    
-    console.log(contact)
-    
-    if (!contact) {
-      res.status(404).json({ error: 'User not found' })
-      return
-    }
-    
     ContactRepository.delete(id)
     res.sendStatus(202)
   }
 }
 
 exports.ContactController = new ContactController()
-
