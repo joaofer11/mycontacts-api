31a9bde5f9449ca8400ecfecdb9e5d0eabeed41e
João Fernandes <joaofergear003@gmail.com>
2023-02-10 22:30:59 -0300

    feat: implement error handler for aplication

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+require('express-async-errors')
 const express = require('express')
 const { router } = require('./router')
 
 const server = express()
-server.use(express.json())
 
+server.use(express.json())
 server.use(router)
+server.use((error, req, res, _) => {
+  console.log(error)
+  res.sendStatus(500)
+})
 
 server.listen(3000, () => console.log('Server started at http://localhost:3000'))
